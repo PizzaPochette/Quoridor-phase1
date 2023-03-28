a9d799aaacfa3c6b09d3b99aa05d20f6342aa06c
PizzaPochette <118787976+PizzaPochette@users.noreply.github.com>
2023-03-27 21:20:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import argparse
 
+
+def analyser_commande():
+    parser = argparse.ArgumentParser(description='Jeu Quoridor - phase d\'initialisation')
+
+    parser.add_argument('idul', metavar='idul', type=str, help='IDUL du joueur')
+    parser.add_argument('-p', '--parties', action='store_true', help='Lister les parties existantes')
+
+    return parser.parse_args()
